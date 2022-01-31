c02a8170aef16cbd5fa7796bf24d33125f23c2f0
Lucky Khumalo <luckyvukani8@gmail.com>
2022-01-31 13:04:38 +0200

    unit tests

---
+++ FileLoggerKata/FileLogger.cs
@@ -0,0 +1,72 @@
+using System;
+using System.IO;
+
+namespace FileLoggerKata
+{
+    public class FileLogger
+    {
+        private const string LogExtension = "txt";
+        private const string LogFileName = "log";
+        private const string WeekendLogFileName = "weekend";
+
+        private IDateProvider DateProvider { get; }
+        private IFileSystem FileSystem { get; }
+
+        public FileLogger()
+        {
+        }
+
+        public FileLogger(IFileSystem fileSystem, IDateProvider dateProvider = null)
+        {
+            DateProvider = dateProvider ?? DefaultDataProvider.Instance;
+            FileSystem = fileSystem;
+        }
+
+        public void Log(string message)
+        {
+            var logFileName = GetLogFileName();
+           
+            if (ShouldRotateWeekendLogs())
+            {
+                ArchiveWeekendLog();
+            }
+
+            if (!FileSystem.Exists(logFileName))
+            {
+                FileSystem.Create(logFileName);
+            }
+
+            FileSystem.Append(logFileName, message);
+
+            bool ShouldRotateWeekendLogs()
+            {
+                return IsWeekend(DateProvider.Today) &&
+                       FileSystem.Exists(logFileName) &&
+                       (DateProvider.Today - FileSystem.GetLastWriteTime(logFileName)).Days > 2;
+            }
+        }
+
+        public string GetLogFileName()
+        {
+            var today = DateProvider.Today;
+
+            return IsWeekend(today)
+                ? $"{WeekendLogFileName}.{LogExtension}"
+                : $"{LogFileName}{ToFileDateFormat(today)}.{LogExtension}";            
+        }
+
+        private void ArchiveWeekendLog()
+        {
+            var lastWriteTime = FileSystem.GetLastWriteTime($"{WeekendLogFileName}.{LogExtension}");
+            var archivedFileName = $"{WeekendLogFileName}-{ToFileDateFormat(lastWriteTime)}.{LogExtension}";
+            FileSystem.Rename($"{WeekendLogFileName}.{LogExtension}", archivedFileName);
+        }
+
+        private static string ToFileDateFormat(DateTime date)
+        {
+            return date.ToString("yyyyMMdd");
+        }
+
+        public static bool IsWeekend(DateTime date) => date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday;
+    }
+}
---
+++ FileLoggerKata/FileLoggerTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoggerKata
+{
+    public class FileLoggerTest
+    {
+        //unit tests.....
+        string fileName = @".\log.txt";
+        FileLogger fileLogger = new FileLogger();
+
+        [Test]
+        public void FileExists()
+        {
+            Assert.IsTrue(File.Exists(fileName));
+        }
+
+        [Test]
+        public void ValidateMessage(string message)
+        {
+            Assert.IsTrue(message.StartsWith(DateTime.Today.ToString()));
+        }
+
+        [Test]
+        public void ValidateFileName()
+        {
+            string date = DateTime.Today.ToString("YYYY-MM-DD:MM:SS");
+            Assert.AreEqual("log" + date + ".txt", fileLogger.GetLogFileName());
+        }
+
+        [Test]
+        public void checkIfWeekend()
+        {
+            DateTime lastSaturday = DateTime.Now.AddDays(-1);
+
+            while (lastSaturday.DayOfWeek != DayOfWeek.Saturday)
+                lastSaturday = lastSaturday.AddDays(-1);
+
+            Assert.IsTrue(File.Exists("weekend" + lastSaturday + ".txt"));
+        }
+    }
+}
